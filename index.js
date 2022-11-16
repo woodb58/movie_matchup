11c4750f2545901f9e1b025692d4e791c13e3ac3
woodb58 <woodb58@gmail.com>
2022-11-16 13:53:35 -0600

    rendering search results

--- index.js
+++ index.js
@@ -5,25 +5,29 @@ const fetchData = async (searchTerm) => {
       s: searchTerm,
     },
   });
-  console.log(response.data);
+
+  if (response.data.Error) {
+    return [];
+  }
+
+  return response.data.Search;
 };
 
 const input = document.querySelector("input");
 
-const debounce = (func, delay = 1000) => {
-  let timeoutId;
-  return (...args) => {
-    if (timeoutId) {
-      clearTimeout(timeoutId);
-    }
-    timeoutId = setTimeout(() => {
-      func.apply(null, args);
-    }, delay);
-  };
-};
+const onInput = async (event) => {
+  const movies = await fetchData(event.target.value);
 
-const onInput = (event) => {
-  fetchData(event.target.value);
+  for (let movie of movies) {
+    const div = document.createElement("div");
+
+    div.innerHTML = `
+    <img src="${movie.Poster}" />
+    <h1>${movie.Title}</h1>
+    `;
+
+    document.querySelector("#target").appendChild(div);
+  }
 };
 
 input.addEventListener("input", debounce(onInput, 500));
